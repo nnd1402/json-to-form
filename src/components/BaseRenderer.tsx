ad70ac24b5cb888030611b66c8cefb093eb6ce51
nnd1402 <nenadmihajlovic1402@gmail.com>
2022-03-15 16:32:40 +0100

    Fix for rendering multiple menus

--- src/components/BaseRenderer.tsx
+++ src/components/BaseRenderer.tsx
@@ -3,11 +3,8 @@ import MenuRenderer from "./MenuRenderer";
 const BaseRenderer = () => {
   var form = require("../data/form.json");
 
-  const holder = JSON.parse(JSON.stringify(form));
-  const menuTitle = holder.menus[0].title;
-
-  return form.menus.map(() => (
-    <MenuRenderer title={menuTitle} key={menuTitle} />
+  return form.menus.map((menu: any) => (
+    <MenuRenderer title={menu.title} key={menu.title} />
   ));
 };
 
